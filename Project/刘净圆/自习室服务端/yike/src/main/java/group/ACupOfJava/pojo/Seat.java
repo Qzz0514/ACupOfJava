142045dd69953dfd37011e5f176e290847b6ad86
yuange123eee <2452994982@qq.com>
2020-11-26 09:37:32 +0800

    add room and seat Class

--- Project/刘净圆/自习室服务端/yike/src/main/java/group/ACupOfJava/pojo/Seat.java
+++ Project/刘净圆/自习室服务端/yike/src/main/java/group/ACupOfJava/pojo/Seat.java
@@ -7,4 +7,81 @@ package group.ACupOfJava.pojo;
  * @Date:2020/11/26 8:35
  */
 public class Seat {
+    private int id;
+    private int user_id;
+    private int room_id;
+    private int row;
+    private int col;
+    private String starttime;
+    private String endtime;
+
+
+    @Override
+    public String toString() {
+        return "Seat{" +
+                "id=" + id +
+                ", user_id=" + user_id +
+                ", room_id=" + room_id +
+                ", row=" + row +
+                ", col=" + col +
+                ", starttime='" + starttime + '\'' +
+                ", endtime='" + endtime + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getRoom_id() {
+        return room_id;
+    }
+
+    public void setRoom_id(int room_id) {
+        this.room_id = room_id;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public String getStarttime() {
+        return starttime;
+    }
+
+    public void setStarttime(String starttime) {
+        this.starttime = starttime;
+    }
+
+    public String getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(String endtime) {
+        this.endtime = endtime;
+    }
 }
